62b6c26263aa466ce2e6e132fdf072ad1ac4559c
Shreya Gupta <41798415+shreeegupta@users.noreply.github.com>
2023-05-17 11:47:03 +0530

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -5,7 +5,7 @@ const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Hello From Shreya",
+        message: "Hello From Shreya..",
       },
       null,
       2
