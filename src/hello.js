c7dbc699b447e779e0fa26ceda81adb50341c5ad
Shreya Gupta <41798415+shreeegupta@users.noreply.github.com>
2023-05-17 11:34:26 +0530

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -5,7 +5,7 @@ const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Hello From Batch 3!",
+        message: "Hello From Shreya",
       },
       null,
       2
